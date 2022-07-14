b80f87addbd4cfed0851d89fff30cd09dd166337
Olga Melman <olga.melman198237@gmail.com>
2022-07-14 10:33:33 +0300

    Added changes in Java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         int ticketPrice = 7685;  // стоимость билета
-        int oneBonus = 20;       // 1 миля = руб
+        int oneMile = 20;       // 1 миля = руб
 
-        int х = ticketPrice / oneBonus;   // Рассчет количества бонусных миль
+        int bonus = ticketPrice / oneMile;   // Рассчет количества бонусных миль
 
-        System.out.println(х + " мили(ль)");
+        System.out.println("Начислено: " + bonus + " мили(ль)");
     }
 }
